94dc869ca7b09fff4727614b0ec058837b305139
朱瑞恒 <18749830459@163.com>
2017-12-12 17:41:02 +0800

    照片墙

--- pages/photos/photos.js
+++ pages/photos/photos.js
@@ -6,12 +6,11 @@ Page({
 
   },
   onLoad: function (options) {
-
+    newName:'朋友照片墙'
   },
   onShow: function () {
-    wx.showToast({
+    wx.showLoading({
       title: '加载中',
-      icon: 'loading'
     })
     let that = this;
     let sign = wx.getStorageSync('sign');
@@ -29,6 +28,36 @@ Page({
           that.setData({
             photosList: res.data.data
           })
+          wx.hideLoading()
+        } else {
+          tips.alert(res.data.msg)
+        }
+        
+      }
+    })
+  },
+  // 新增相册
+  newPhotos(e){
+    wx.showLoading({
+      title: '加载中',
+    })
+    let that = this;
+    let sign = wx.getStorageSync('sign');
+    // 请求 
+    wx.request({
+      url: apiurl + "photo/create-new-wall?sign=" + sign + '&operator_id=' + app.data.kid,
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET",
+      success: function (res) {
+        console.log("新增照片墙:", res);
+        var status = res.data.status;
+        if (status == 1) {
+          wx.setStorageSync('pw_id', res.data.data);
+          wx.switchTab({
+            url: '../indexs/indexs',
+          })
 
         } else {
 
@@ -37,9 +66,39 @@ Page({
       }
     })
   },
-  // 新增相册
-  newPhotos(){
-    
+  // 新增照片名称
+  niceName(e){
+    this.setData({ 
+      newName: e.detail.value
+    })
+  },
+  setName(e){
+    let that = this;
+    let sign = wx.getStorageSync('sign');
+    let id = e.currentTarget.dataset.pw_id;
+    // 请求 
+    wx.request({
+      url: apiurl + "photo/edit-pname?sign=" + sign + '&operator_id=' + app.data.kid,
+      data:{
+        pw_id:id,
+        name: e.detail.value
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET",
+      success: function (res) {
+        console.log("编辑照片墙名称:", res);
+        var status = res.data.status;
+        if (status == 1) {
+          tips.success('编辑成功');
+
+        } else {
+          tips.alert(res.data.msg);
+        }
+        wx.hideLoading()
+      }
+    })
   }
 
 })
