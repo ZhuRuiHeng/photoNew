3ac7b31cd29ccd1c0699bc5ea90bdc6f4ebdef17
朱瑞恒 <18749830459@163.com>
2017-12-16 18:06:40 +0800

    新增

---
+++ pages/inform/inform.js
@@ -0,0 +1,18 @@
+// pages/inform/inform.js
+Page({
+
+  data: {
+    userInfo: wx.getStorageSync('userInfo'),
+    url: 'https://friend-guess.playonwechat.com/assets/images/result/40741d60add2279916d8783b3d6667f9.jpg?1513410944?0.5924372259162527'
+  },
+  onLoad: function (options) {
+  
+  },
+  onReady: function () {
+  
+  },
+  onShow: function () {
+  
+  },
+
+})
---
+++ pages/square/square.js
@@ -0,0 +1,16 @@
+// pages/square/square.js
+Page({
+  data: {
+    userInfo: wx.getStorageSync('userInfo'),
+    url:'https://friend-guess.playonwechat.com/assets/images/result/40741d60add2279916d8783b3d6667f9.jpg?1513410944?0.5924372259162527'
+  },
+  onLoad: function (options) {
+  
+  },
+  onShow: function () {
+  
+  },
+
+
+
+})
